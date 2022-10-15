1decdd02a71d9db4c4a99912b3e02fe8e4fc98d9
khalilannbiya <syeichkhalil@gmail.com>
2022-10-15 08:49:12 +0700

    feat: Get Detail Books

--- src/handler.js
+++ src/handler.js
@@ -86,4 +86,26 @@ const getAllBooksHandler = () => ({
   },
 });
 
-export { addBookHandler, getAllBooksHandler };
+const getBookByIdHandler = (request, h) => {
+  const { bookId } = request.params;
+
+  const book = books.filter((b) => b.id === bookId)[0];
+
+  if (book !== undefined) {
+    return {
+      status: 'success',
+      data: {
+        book,
+      },
+    };
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
+export { addBookHandler, getAllBooksHandler, getBookByIdHandler };
